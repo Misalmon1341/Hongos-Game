4649cf30d6b961f8e836b0a5bc589d2e44533c8f
Misael Bautista <misagamer559@gmail.com>
2025-03-25 00:11:55 -0600

    Recoleccion de items

---
+++ Funji-Game/Assets/CarpetaMisael/ScriptsMisa/PlayerScripts/ActiveGunPlayer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveGunPlayer : MonoBehaviour
+{
+    public TakeGuns takeGuns;
+    public int numeroArma;
+
+    private void Start()
+    {
+        takeGuns = GameObject.FindGameObjectWithTag("Player").GetComponent<TakeGuns>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            takeGuns.ActiveGuns(numeroArma);
+            Destroy(gameObject);
+        }
+    }
+}

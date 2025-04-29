40f972a8a92de5aa9f640f6da8da85db623f131b
Misael Bautista <misagamer559@gmail.com>
2025-04-28 22:21:24 -0600

    walljump

--- Funji-Game/Assets/AssetsVic/Codigo/MovPersonaje.cs
+++ Funji-Game/Assets/AssetsVic/Codigo/MovPersonaje.cs
@@ -30,6 +30,13 @@ public class MovPersonaje : MonoBehaviour
     [Header("Buffer")]
     private float tiempoBufferSalto = 0f;
     private float duracionBufferSalto = 0.1f;
+    [Header("WallJump")]
+    private bool enLaPared = false;
+    private Vector3 posicionRayCast;
+    private float centradoRayCast = 0.5f;
+    public LayerMask ParedLayerMask;
+    private float saltoParedLateral = 18f;
+    private float separacionParedSalto = 2.5f;
 
     void Start()
     {
@@ -45,16 +52,21 @@ public class MovPersonaje : MonoBehaviour
     void moverPersonaje()
     {
         hInput = Input.GetAxisRaw("Horizontal");
-        movimiento.x = hInput * velocidad;
+        if (!enLaPared)
+        {
+            movimiento.x = hInput * velocidad;
+        }
         if (controlPersonaje.isGrounded)
         {
             saltoDoble = false;
             enElAire = false ;
-            dashActivo = false;    
+            dashActivo = false;
+            enLaPared = false ;
             coyoteActivo= true;
             animacion.SetBool("Jumping", false);
             animacion.SetBool("Running", false);
             animacion.SetBool("Dash", false);
+            animacion.SetBool("Pared", false);
             if (tiempoBufferSalto > 0)
             {
                 Salto();
@@ -62,6 +74,8 @@ public class MovPersonaje : MonoBehaviour
         }
         else
         {
+             enElAire = true;
+            ComprobarColisionPared();
             if (coyoteActivo)
             {
                 coyoteActivo = false;
@@ -70,7 +84,6 @@ public class MovPersonaje : MonoBehaviour
             }
             if  (tiempoCoyote + duracionCoyoteTime < Time.time)
             {
-                enElAire = true;
                 if (saltoDoble && Input.GetButtonDown("Jump"))
                 {
                     saltoDoble = false;
@@ -106,20 +119,12 @@ public class MovPersonaje : MonoBehaviour
     IEnumerator Dash()
     {
         float tiempoInicial = Time.time;
-        float direccion;
         dashActivo = true;
-        if(rotacionPersonaje.y > 0)
-        {
-            direccion = 1f;
-        }
-        else
-        {
-            direccion = -1f;
-        }
+       
         while (Time.time < tiempoInicial + duracionDash)
         {
             animacion.SetBool("Dash", true);
-            movimiento.x = direccion*velocidadDash;
+            movimiento = this.transform.TransformDirection(Vector3.forward * velocidadDash);
             movimiento.y = 0;
             controlPersonaje.Move(movimiento * Time.deltaTime);
             yield return null;
@@ -127,12 +132,54 @@ public class MovPersonaje : MonoBehaviour
     }
     void Salto()
     {
+        ComprobarColisionPared();
         enElAire = true;
         saltoDoble = true;
         coyoteActivo = false;
+        if (enLaPared)
+        {
+            rotacionPersonaje = Quaternion.LookRotation(this.transform.TransformDirection(Vector3.forward * -1));
+            this.transform.rotation = rotacionPersonaje;
+            movimiento = this.transform.TransformDirection(Vector3.forward * saltoParedLateral * velocidad);
+        }
+        enLaPared = false;
+        animacion.SetBool("Pared", false);
         tiempoCoyote -= duracionCoyoteTime;
         animacion.SetBool("Jumping", true);
         movimiento.y = fuerzaSalto;
         tiempoBufferSalto = 0f;
+    }
+    void ComprobarColisionPared()
+    {
+        float longitudRaycast = 0.3f;
+        posicionRayCast = this.transform.position;
+        posicionRayCast.y += centradoRayCast;
+        if (Physics.Raycast(posicionRayCast,this.transform.TransformDirection(Vector3.forward), longitudRaycast,ParedLayerMask))
+        {
+            if (controlPersonaje.isGrounded)
+            {
+                movimiento = this.transform.TransformDirection(Vector3.forward) * separacionParedSalto * velocidad * -1;
+            }
+            else
+            {
+                animacion.SetBool("Jumping", false);
+                animacion.SetBool("Running", false);
+                animacion.SetBool("Pared", true);
+                if (!enLaPared)
+                {
+                    movimiento.y = 0;
+                }
+                enLaPared = true;
+                enElAire = false;
+                gravedad = 1.8f;
+            }
+        }
+        else
+        {
+            enLaPared = false;
+            animacion.SetBool("Pared", false);
+            gravedad = 9.8f;
+        }
+
     }
 }

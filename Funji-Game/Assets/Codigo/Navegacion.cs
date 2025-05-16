3a194914524aa6cf5cee737d3db64f2fe78fe8ab
VicBojoDev10 <reybojorgejr@hotmail.com>
2025-05-15 18:42:14 -0600

    cambio de escenas

---
+++ Funji-Game/Assets/Codigo/Navegacion.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Navegacion : MonoBehaviour
+{
+    public void Jugar()
+    {
+        //Aquí va la escena del juego
+    }
+    public void Ajustes()
+    {
+        SceneManager.LoadScene("Ajustes");
+    }
+    public void Creditos()
+    {
+        SceneManager.LoadScene("Creditos");
+    }
+}
